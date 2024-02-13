217adb55c81d5a7bc9710ecad9c1c2eee36e2e5d
Felipe Versiane <felipeversiane09@gmail.com>
2024-02-13 00:52:42 -0300

    feat: base user model

---
+++ src/account/views.py
@@ -0,0 +1,5 @@
+from account.endpoints.custom_obtain_pair import *
+from account.endpoints.custom_provider_auth import *
+from account.endpoints.custom_verify import *
+from account.endpoints.custom_refresh import *
+from account.endpoints.logout import *
