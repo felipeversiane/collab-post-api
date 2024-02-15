9cfc109ad38a464e92dddd280c81a10c0531fa43
Felipe Versiane <felipeversiane09@gmail.com>
2024-02-15 15:00:38 -0300

    changing proposal model and urls

--- src/app/entities/proposal.py
+++ src/app/entities/proposal.py
@@ -7,6 +7,7 @@ import uuid
 
 SITUATION_CHOICES = [
     ("W",_("Waiting")),
+    ("I",_("Interest")),
     ("S",_("Signed")),
 ]
 
@@ -20,13 +21,25 @@ class Proposal(models.Model):
     situation = models.CharField(max_length=1,default='W',null=False, blank=False,choices=SITUATION_CHOICES,verbose_name=_('Situation'),validators=[validate_first_letter])
     is_paid = models.BooleanField(default=False,null=False, blank=False)
     discounted_value = models.DecimalField(max_digits=10, null=False, blank=False, decimal_places=2, verbose_name=_('Discounted Value'),validators=[validate_value])
+    accepted_at = models.DateTimeField(null=True, blank=True, verbose_name=_('Accepted Date'))
+    paid_date = models.DateTimeField(null=True, blank=True, verbose_name=_('Paid Date'))
 
     def calculate_discounted_value(self):
         discount_percentage = 0.05
         discounted_value = self.budget * (1 - discount_percentage)
         return discounted_value
 
+    def proposal_accepted(self): 
+        return self.situation == 'S' and not self.accepted_at;
+
+    def is_paid(self):
+        return self.is_paid == True and not self.paid_date;
+
     def save(self, *args, **kwargs):
+        if self.proposal_accepted(self):  
+            self.accepted_at = timezone.now()
+        if self.is_paid(self):
+            self.paid_date = timezone.now()
         self.discounted_value = self.calculate_discounted_value()
         super().save(*args, **kwargs)
 
--- src/app/urls.py
+++ src/app/urls.py
@@ -4,8 +4,8 @@ from django.conf.urls import include
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-
-
+router.register(r'project',views.ProjectViewSet,basename='project')
+router.register(r'proposal',views.ProposalViewSet,basename='proposal')
 
 urlpatterns = [
     re_path('', include(router.urls)),
