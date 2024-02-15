a130b54a4098ded4fa53ad5f9b751664a712bd77
Felipe Versiane <felipeversiane09@gmail.com>
2024-02-15 13:35:57 -0300

    feat: project viewset

---
+++ src/app/endpoints/project_views.py
@@ -0,0 +1,59 @@
+from rest_framework.decorators import permission_classes,action
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import PermissionDenied
+from app.entities.project import Project
+from app.serializers.project_serializers import ProjectSerializer
+from rest_framework import viewsets,status
+from django.utils.translation import gettext_lazy as _
+from rest_framework.permissions import AllowAny
+
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+    def get_permissions(self):
+        if self.action != 'list':
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [AllowAny]
+        return [permission() for permission in permission_classes]
+
+    def list(self,request,*args, **kwargs):
+        queryset = self.queryset.exclude(situation__in=['W', 'F'])  
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    
+    def read(self, instance,*args, **kwargs):
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+
+    def destroy(self, instance,*args, **kwargs):
+        if instance.user != self.request.user:
+                raise PermissionDenied({"message": _("Permission denied.")})
+        instance.delete()
+        return Response({"message":_("Education deleted sucessfully.")},status=status.HTTP_204_NO_CONTENT)
+    
+    def create(self, request,*args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(user=request.user)
+        return Response({"message":_("Job created successfully.")},status=status.HTTP_201_CREATED)
+
+    def update(self, serializer,*args, **kwargs):
+        item = self.get_object()
+        if item.user != self.request.user:
+            raise PermissionDenied({"message": _("Permission denied.")})
+        serializer.is_valid(raise_exception=True)        
+        serializer.save()
+        return Response({"message":_("Education changed sucessfully.")},status=status.HTTP_204_NO_CONTENT)
+    
+    def partial_update(self,serializer,*args, **kwargs):
+        item = self.get_object()
+        if item.user != self.request.user:
+            raise PermissionDenied({"message": _("Permission denied.")})
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({"message":_("Education changed sucessfully.")},status=status.HTTP_204_NO_CONTENT)
+    
