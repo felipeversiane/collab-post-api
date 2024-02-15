32fd770d17f0258c55aaefafa967b12e7b04ae86
Felipe Versiane <felipeversiane09@gmail.com>
2024-02-15 13:35:36 -0300

    change the validator return

--- src/account/utils/validators.py
+++ src/account/utils/validators.py
@@ -10,38 +10,34 @@ from django.utils.translation import gettext_lazy as _
 PHONE_REGEX = r"^\d{11}$"
 DATE_REGEX = r'(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$'
 
-#Functions
 def validate_first_letter(str):    
     if str[0].isdigit():        
-        raise ValidationError(_("First letter cannot be a number."))
+        raise ValidationError({'message': _("First letter cannot be a number.")}, code='invalid')
 
 def validate_letters(name):    
     if not re.match(r'^[A-Za-z ]+$', name):      
-        raise ValidationError(_("Only letters and spaces allowed."))
+        raise ValidationError({'message': _("Only letters and spaces allowed.")}, code='invalid')
         
 def validate_phone(phone):
     if not re.match(PHONE_REGEX, phone):
-        raise ValidationError(_("Invalid phone number format.")) 
+        raise ValidationError({'message': _("Invalid phone number format.")}, code='invalid') 
 
 def validate_value(value):
     if value <= 0:
-        raise ValidationError(_("Value must be positive."))
+        raise ValidationError({'message': _("Value must be positive.")}, code='invalid')
         
 def validate_date(date): 
     if date < timezone.now().date():  
-        raise ValidationError(_("Invalid date."))
+        raise ValidationError({'message': _("Invalid date.")}, code='invalid')
         
 def validate_time(time):       
     if time < timezone.now().time():   
-        raise ValidationError(_("Invalid time."))    
+        raise ValidationError({'message': _("Invalid time.")}, code='invalid')    
     
 def validate_birth_date(value):
     if not re.match(DATE_REGEX, value):
-         raise ValidationError(_("Invalid date of birth."))
+         raise ValidationError({'message': _("Invalid date of birth.")}, code='invalid')
     
 def validate_safe_text(value):
     if re.search(r'<|>|&|;|\'|"|\/|\\', value):
-        raise ValidationError(
-            _("The text cannot contain dangerous special characters."),
-            code='invalid',
-        )
+        raise ValidationError({'message': _("The text cannot contain dangerous special characters.")}, code='invalid')
--- src/app/utils/validators.py
+++ src/app/utils/validators.py
@@ -7,52 +7,37 @@ from django.utils.translation import gettext_lazy as _
 
 #Regex 
 PHONE_REGEX = r"^\d{11}$"
-ZIPCODE_REGEX = r"^\d{5}-\d{3}$"
 DATE_REGEX = r'(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$'
 
 
 def validate_first_letter(str):    
     if str[0].isdigit():        
-        raise ValidationError(_("First letter cannot be a number."))
+        raise ValidationError({'message': _("First letter cannot be a number.")}, code='invalid')
 
 def validate_letters(name):    
     if not re.match(r'^[A-Za-z ]+$', name):      
-        raise ValidationError(_("Only letters and spaces allowed."))
+        raise ValidationError({'message': _("Only letters and spaces allowed.")}, code='invalid')
         
 def validate_phone(phone):
     if not re.match(PHONE_REGEX, phone):
-        raise ValidationError(_("Invalid phone number format.")) 
+        raise ValidationError({'message': _("Invalid phone number format.")}, code='invalid') 
 
 def validate_value(value):
     if value <= 0:
-        raise ValidationError(_("Value must be positive."))
+        raise ValidationError({'message': _("Value must be positive.")}, code='invalid')
         
 def validate_date(date): 
     if date < timezone.now().date():  
-        raise ValidationError(_("Invalid date."))
+        raise ValidationError({'message': _("Invalid date.")}, code='invalid')
         
 def validate_time(time):       
     if time < timezone.now().time():   
-        raise ValidationError(_("Invalid time."))   
-
-def validate_zipcode(zipcode):    
-    if not re.match(ZIPCODE_REGEX, zipcode):    
-        raise ValidationError(_("Invalid zipcode format."))
-    
-def validate_audio_file(value):
-    import os
-    ext = os.path.splitext(value.name)[1]  
-    if not ext.lower() in ['.wav','.mp3','.ogg']:
-        raise ValidationError(_("Only audio files are allowed."))
-    
+        raise ValidationError({'message': _("Invalid time.")}, code='invalid')    
     
 def validate_birth_date(value):
     if not re.match(DATE_REGEX, value):
-         raise ValidationError(_("Invalid date of birth."))
+         raise ValidationError({'message': _("Invalid date of birth.")}, code='invalid')
     
 def validate_safe_text(value):
     if re.search(r'<|>|&|;|\'|"|\/|\\', value):
-        raise ValidationError(
-            _("The text cannot contain dangerous special characters."),
-            code='invalid',
-        )
+        raise ValidationError({'message': _("The text cannot contain dangerous special characters.")}, code='invalid')
