61f37804942560dc9c0a5ebe687935f21b38543d
Felipe Versiane <felipeversiane09@gmail.com>
2024-02-22 23:34:15 -0300

    fix: deleting the sphinx build

--- src/account/models.py
+++ src/account/models.py
@@ -1,1 +1,4 @@
 from account.entities.user_account import UserAccount
+from account.entities.job import Job
+from account.entities.education_background import EducationalBackground as Education
+from account.entities.competence import Competence
